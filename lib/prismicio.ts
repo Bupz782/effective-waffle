eaf2e257fd7a5205538ba5294fdce8102303b989
arthur.bertier <arthur.bertier@emeria.eu>
2026-03-24 11:19:55 +0100

    fix(update) prismicio

--- lib/prismicio.ts
+++ lib/prismicio.ts
@@ -1,14 +1,42 @@
-import { createClient as baseCreateClient, type ClientConfig } from "@prismicio/client";
+import {
+  createClient as baseCreateClient,
+  type ClientConfig,
+  type Route,
+} from "@prismicio/client";
+import { enableAutoPreviews } from "@prismicio/next";
+import sm from "../slicemachine.config.json";
 
-export const repositoryName = process.env.PRISMIC_REPOSITORY_NAME || "";
+/**
+ * The project's Prismic repository name.
+ */
+export const repositoryName =
+  process.env.NEXT_PUBLIC_PRISMIC_ENVIRONMENT || sm.repositoryName;
 
-export function createClient(config: ClientConfig = {}) {
+/**
+ * A list of Route Resolver objects that define how a document's `url` field is resolved.
+ *
+ * {@link https://prismic.io/docs/route-resolver#route-resolver}
+ */
+// TODO: Update the routes array to match your project's route structure.
+const routes: Route[] = [];
+
+/**
+ * Creates a Prismic client for the project's repository. The client is used to
+ * query content from the Prismic API.
+ *
+ * @param config - Configuration for the Prismic client.
+ */
+export const createClient = (config: ClientConfig = {}) => {
   const client = baseCreateClient(repositoryName, {
-    fetchOptions: {
-      next: { tags: ["prismic"] },
-    },
+    routes,
+    fetchOptions:
+      process.env.NODE_ENV === "production"
+        ? { next: { tags: ["prismic"] }, cache: "force-cache" }
+        : { next: { revalidate: 5 } },
     ...config,
   });
 
+  enableAutoPreviews({ client });
+
   return client;
-}
+};
